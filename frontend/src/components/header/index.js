3aadf3f4e622242815b1ab2b5bfb0ff80f2a21be
Henry Nguyen <s3560635@student.rmit.edu.au>
2022-10-03 10:49:23 +1100

    added admin panel, routing and show state

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 import Navbar from './Navbar'
 const Header = () => {
+  const [show, setShow] = useState(true)
+
+  const location = useLocation()
+  const routeName = location.pathname.split('/')[1]
+
+  useEffect(() => {
+    if (routeName === 'adminpanel') {
+      setShow(false)
+    } else {
+      setShow(true)
+    }
+  }, [routeName])
+
   return (
     <div>
 
-        <Navbar/>
+      {show && <Navbar />}
         
 
     </div>
